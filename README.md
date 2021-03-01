9f9cc9e82ef712707a2a9ab1df578ee6137636a3
Prototype-i <kevinsimard42@gmail.com>
2021-02-28 20:19:49 -0500

    Added functions that will return strings from arrays, update to README

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Mixed Messages Project #
-  ##  **~Find Out What the Future Holds~** ##
+#  **Mixed Messages Project - Find Out What the Future Holds** #
 ###  Project Description
 This program will generate a random prediction for the future when run from a bash terminal
 
 ### **Return** Details
 * The year for the prediction is random
 * The prediction will not involve the person calling the command
+* You will **only** get predictions of events that will happen in 5 or more years
 * The prediction will always be 100% accurate (unless proven otherwise)
   
--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const whatTheFutureHolds = {
-    people: ['Bob Saget', 'Elon Musk', 'Charlie Sheen', 'Donald Trump', 'Barak Obama', 'Rick Astley', ],
+    people: ['Bob Saget', 'Elon Musk', 'Charlie Sheen', 'Donald Trump', 'Barak Obama', 'Rick Astley', 'George Hotz',],
     places: ['Atlantis', 'New New York', 'Rapture', 'Avalon', 'ValHala', 'Mars (The Planet)', 'West Philidalphia'],
     things: ['Tower of Pisa', 'Decleration of Independence', 'Mona Lisa', 'A Plastic Fork', 'A Grizzly Bear'],
+    randomPerson() {
+        //Returns random person from 'people' array, no limit on array length.
+        const index = Math.floor(Math.random() * this.people.length);
+        return this.people[index]
+    },
+    randomPlace() {
+        //Returns random person from 'places' array, no limit on array length.
+        const index = Math.floor(Math.random() * this.places.length);
+        return this.places[index]
+    },
+    randomThing() {
+        //Returns random person from 'things' array, no limit on array length.
+        const index = Math.floor(Math.random() * this.things.length);
+        return this.things[index]
+    },
 } 
+
+
+
+//console.log(whatTheFutureHolds.randomPerson())
