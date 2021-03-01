709648dfe4a4d598b081b0a7326caae8e775daf0
Prototype-i <kevinsimard42@gmail.com>
2021-02-28 23:46:52 -0500

    FINAL - added final return string, reordered arrays, added getter/setters and update on README

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
-#  **Mixed Messages Project - Find Out What the Future Holds** #
-###  Project Description
-This program will generate a random prediction for the future when run from a bash terminal
+# **Find Out What the Future Holds** #
+___
+## *Mixed Messages Project*
+***
 
-### **Return** Details
+##  Project Description
+This program will generate a random prediction for the future.
+
+## Input options
+* `whatTheFutureHolds.predict()`
+  
+  Creates a new prediction for the future
+
+* `whatTheFutureHolds.people(*person*)`
+  
+    where *person* = new person to include on list of people who's future may be predicted
+
+* `whatTheFutureHolds.things(*thing*)`
+  
+    where *thing* = add objects to prediction
+
+* `whatTheFutureHolds.places(*place*)`
+    
+    where *place* = creates new possibility for where prediction will happen
+
+* `whatTheFutureHolds.reasons(*reason*)`
+
+    werer *reason* = give sombody in the future a new reason for doing something
+
+## **Return** Details
 * The year for the prediction is random
 * The prediction will not involve the person calling the command
-* You will **only** get predictions of events that will happen in 5 or more years
+* **All** events predicted will happen within 5 to 2000 years
 * The prediction will always be 100% accurate (unless proven otherwise)
   
--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 const whatTheFutureHolds = {
-    people: ['Bob Saget', 'Elon Musk', 'Charlie Sheen', 'Donald Trump', 'Barak Obama', 'Rick Astley', 'George Hotz',],
-    places: ['Atlantis', 'New New York', 'Rapture', 'Avalon', 'ValHala', 'Mars (The Planet)', 'West Philidalphia'],
-    things: ['Tower of Pisa', 'Decleration of Independence', 'Mona Lisa', 'A Plastic Fork', 'A Grizzly Bear'],
-    reasons: ['In the name of science', 'To bring about a new age of prosperity', 'Just for the heck of it', ''],
+    _people: ['Bob Saget', 'Elon Musk', 'Charlie Sheen', 'Donald Trump', 'Barak Obama', 'Batman', 'Rick Astley', 'George Hotz',],
+    _things: ['A freshly plucked plum', 'the Decleration of Independence', 'the Mona Lisa', 'A Plastic Fork', 'A Grizzly Bear'],
+    _places: ['Atlantis', 'New New York', 'Rapture', 'Avalon', 'Val-Hala', 'Mars (The Planet)', 'West Philidalphia'],
+    _reasons: ['in the name of science', 'to bring about a new age of prosperity', 'just for the heck of it', 'because they saw it on the internet', 'for justice', 'for legal reasons'],
+    //Getters and Setters for possible outcomes
+    set people(person){
+        if(typeof person === 'String'){this.people.push(person)}
+    },
+    get people(){
+        return this._people
+    },
+    set things(thing){
+        if(typeof thing === 'String'){this.things.push(thing)}
+    },
+    get things(){
+        return this._things
+    },
+    set places(place){
+        if(typeof place === 'String'){this.places.push(place)}
+    },
+    get places(){
+        return this._places
+    },
+    set reasons(reason){
+        if(typeof reason === 'String'){this.reasons.push(reason)}
+    },
+    get reasons() {
+        return this._reasons
+    },
+    //Randomly selects possible outcomes
     randomPerson() {
         //Returns random person from 'people' array, no limit on array length.
         const index = Math.floor(Math.random() * this.people.length);
         return this.people[index]
     },
-    randomPlace() {
-        //Returns random person from 'places' array, no limit on array length.
-        const index = Math.floor(Math.random() * this.places.length);
-        return this.places[index]
-    },
     randomThing() {
         //Returns random person from 'things' array, no limit on array length.
         const index = Math.floor(Math.random() * this.things.length);
         return this.things[index]
+    },
+    randomPlace() {
+        //Returns random person from 'places' array, no limit on array length.
+        const index = Math.floor(Math.random() * this.places.length);
+        return this.places[index]
     },
     randomReason() {
         //Returns random person from 'reasons' array, no limit on array length.
@@ -29,7 +55,10 @@ const whatTheFutureHolds = {
         const futureDate = new Date(Math.floor(Math.random() * limit) + 2000, Math.floor(Math.random() * 12), Math.floor(Math.random() * 31)).toDateString()
         return futureDate
     },
-
-}
-console.log(whatTheFutureHolds.getDate())
-//console.log(whatTheFutureHolds.randomPerson())
+    //This section affects output
+    predict() {
+        //returns prediction using random elements from this object's arrays 
+        console.log(`A very Interesting future awaits, On the exact date of ${this.getDate()} the person known as ${this.randomPerson()}, will be entrusted with taking ${this.randomThing()} to a mysterious land known as ${this.randomPlace()} ${this.randomReason()}.`)
+    }
+};
+whatTheFutureHolds.predict()
