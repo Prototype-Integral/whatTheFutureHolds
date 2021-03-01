a12063d2cfdb597f26a500b0ceb4b301c8ea4668
Prototype-i <kevinsimard42@gmail.com>
2021-02-28 22:35:17 -0500

    Added "reasons" array and randomizer function. Added getDate function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const whatTheFutureHolds = {
     people: ['Bob Saget', 'Elon Musk', 'Charlie Sheen', 'Donald Trump', 'Barak Obama', 'Rick Astley', 'George Hotz',],
     places: ['Atlantis', 'New New York', 'Rapture', 'Avalon', 'ValHala', 'Mars (The Planet)', 'West Philidalphia'],
     things: ['Tower of Pisa', 'Decleration of Independence', 'Mona Lisa', 'A Plastic Fork', 'A Grizzly Bear'],
+    reasons: ['In the name of science', 'To bring about a new age of prosperity', 'Just for the heck of it', ''],
     randomPerson() {
         //Returns random person from 'people' array, no limit on array length.
         const index = Math.floor(Math.random() * this.people.length);
@@ -17,8 +18,18 @@ const whatTheFutureHolds = {
         const index = Math.floor(Math.random() * this.things.length);
         return this.things[index]
     },
-} 
+    randomReason() {
+        //Returns random person from 'reasons' array, no limit on array length.
+        const index = Math.floor(Math.random() * this.reasons.length);
+        return this.reasons[index]
+    },
+    getDate() {
+        const today = new Date()
+        const limit = today.getFullYear()
+        const futureDate = new Date(Math.floor(Math.random() * limit) + 2000, Math.floor(Math.random() * 12), Math.floor(Math.random() * 31)).toDateString()
+        return futureDate
+    },
 
-
-
+}
+console.log(whatTheFutureHolds.getDate())
 //console.log(whatTheFutureHolds.randomPerson())
